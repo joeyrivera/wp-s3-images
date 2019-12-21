5e117bf33ca627db61cbcd1b14f21ce91d1da8e3
Joey Rivera <joey1.rivera@gmail.com>
2019-12-21 18:28:08 +0000

    settings page working

--- index.php
+++ index.php
@@ -1,16 +1,37 @@
 <?php
+
 /**
- * Plugin Name: JR Image Upload
+ * Plugin Name: JR S3 Images
  * Plugin URI: http://www.mywebsite.com/my-first-plugin
- * Description: Allows admin to upload images to S3
- * Version: 1.1
+ * Description: View and upload images in S3 bucket
+ * Version: 1.0
  * Author: Joey Rivera
  * Author URI: http://www.joeyrivera.com
  */
 
-require_once 'Jr_S3_Image_List.php';
+require_once 'widget.php';
 
-// register My_Widget
-add_action( 'widgets_init', function(){
-	register_widget( 'Jr_S3_Image_List' );
+$widget = new \Jr\S3\Images\Widget();
+
+// register widget
+add_action('widgets_init', function () use ($widget) {
+    register_widget($widget);
+});
+
+add_action('admin_init', function () use ($widget) {
+    register_setting('jrs3images_option_group', 'bucket_name');
+    register_setting('jrs3images_option_group', 'bucket_region');
+    register_setting('jrs3images_option_group', 'identity_pool_id_public');
+    register_setting('jrs3images_option_group', 'identity_pool_id_admin');
+});
+
+// register settings
+add_action('admin_menu', function () use ($widget) {
+    add_options_page(
+        'Jr S3 Images',
+        'Jr S3 Images',
+        'administrator',
+        'jrs3imagessettings',
+        [$widget, 'create_plugin_settings_page']
+    );
 });
